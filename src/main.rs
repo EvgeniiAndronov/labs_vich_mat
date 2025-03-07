34b969b4c4c44855a11ef0d30a93b707abba6c7e
Ged <120242971+GedK0tlet@users.noreply.github.com>
2025-03-07 11:20:30 +0300

    add simple test

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
-use std::time::{Instant};
-
-mod lagrange;
 mod nuton;
 
 fn main() {
-    let x = vec![0.0, 2.0, 4.0, 6.0];
-    let y = vec![-5.0, -3.0, 31.0, 145.0];
+    let x = vec![15.0, 20.0, 25.0, 30.0, 35.0, 40.0];
+    let y = vec![0.2588, 0.3420, 0.4226, 0.5, 0.5736, 0.6428];
 
-    let value_to_interpolate: f32 = 1.0;
+    let value_to_interpolate: f32 = 56.0;
 
     let res_n = nuton::nuton(&value_to_interpolate, &x, &y);
     println!("Nuton interpolation polinom \t- ({};{:?})", value_to_interpolate, res_n);
 
-    let res_l = lagrange::lagrange(value_to_interpolate, &x, &y);
-    println!("Lagrange polinom interpolation \t- ({};{:?})", value_to_interpolate, res_l);
 }
